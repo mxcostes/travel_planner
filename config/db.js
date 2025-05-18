95418613ec92c10840faadd97591e97ce707bb42
Max Costes <maxcostes@Maxs-Laptop.local>
2025-05-18 10:43:40 -0400

    deployment prep pool

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST || process.env.MYSQLHOST || 'localhost',
     user: process.env.DB_USER || process.env.MYSQLUSER || 'root',
     password: process.env.DB_PASSWORD || process.env.MYSQL_ROOT_PASSWORD,
     database: process.env.DB_NAME || process.env.MYSQLDATABASE,
-    port: process.env.DB_PORT || process.env.MYSQLPORT || 3306
+    port: process.env.DB_PORT || process.env.MYSQLPORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 db.connect((err) => {
@@ -17,7 +20,7 @@ db.connect((err) => {
     console.log('✅ Connected to MySQL Database');
 });
 
-module.exports = db;
+module.exports = db.promise();
 
 // require('dotenv').config();
 
